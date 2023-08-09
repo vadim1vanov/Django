4e7361059e23f7cc3123fb863e65d9f327453504
Vadim Ivanov <fr0net@yandex.ru>
2023-08-09 16:09:27 +0300

    first_commit

---
+++ DjangoEx/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+data = {
+    'title': 'Главная страница',
+    'values': ['Some', 'Hello', '123']
+}
+# Create your views here.
+def index(requests):
+    return render(requests, 'main/index.html', data)
+
+def about(requests):
+    return render(requests, "main/about.html")
+
---
+++ DjangoEx/news/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Articles(models.Model):
+    title = models.CharField('Название', max_length=64, default='Новость #XXX')
+    anons = models.CharField('Анонс', max_length=250, default='Анонс #XXX')
+    full_text = models.TextField('Статья', default='Статья #XXX')
+    date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
