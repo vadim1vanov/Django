8ac1a2a3a25d1492229a8dd8af8d20d94ad73594
Vadim Ivanov <fr0net@yandex.ru>
2023-08-11 15:28:31 +0300

    commit_3

--- DjangoEx/news/views.py
+++ DjangoEx/news/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
+
 
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
-
+def create(request):
+    error = ''
+    if request.method == "POST":
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_home')
+        else:
+            error = 'Form is incorrect!'
+    form = ArticlesForm
+    data = {
+        'form': form,
+        'error': error,
+    }
+    return render(request, 'news/create.html', data)
